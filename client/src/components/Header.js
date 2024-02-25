d334969d6f180af0e637b7e2525d76c46184f823
ronit127 <00ronitr@gmail.com>
2024-02-25 02:55:05 -0600

    UI rehaul

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -9,10 +9,10 @@ const Header = () => {
       
       <nav>
         <img className="logo" src="assets/logo3.png" />
-        <a href="#"><h1>Home</h1></a>
-        <a href="#"><h1>Upload</h1></a>
+        <a href="#"><h1>STUDY_BUD</h1></a>
+        {/* <a href="#"><h1>Upload</h1></a>
         {/* <h1>Practice</h1> */}
-        <a href="#"><h1>Study Plan</h1></a>
+        {/* <a href="#"><h1>Study Plan</h1></a> */} 
       </nav>
 {/* 
       <div className="image-container">
